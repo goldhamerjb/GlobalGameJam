dcafb39f9fdae17e661deb5a53d9915f1f8a061a
goldhamerjb <goldhamerjb@vcu.edu>
2016-01-30 10:43:49 -0500

    no message

--- Assets/Indicator.cs
+++ Assets/Indicator.cs
@@ -3,9 +3,21 @@ using System.Collections;
 
 public class Indicator : MonoBehaviour {
     GameObject collidedWith = null;
+    public GameObject staticIndicator;
+    //public string str;
+    float foo = 0;
+
+    float amplitude=0;
+    float frequency=0;
+
+    bool move = false;
+
+    int directionScalar = 1; //equals 1 when to the right, -1 when to the left
+
+
 	// Use this for initialization
 	void Start () {
-        
+        StartCoroutine("StartMove");
 	
 	}
 
@@ -22,7 +34,17 @@ public class Indicator : MonoBehaviour {
 
     void FixedUpdate()
     {
-        gameObject.transform.localPosition = new Vector2(gameObject.transform.localPosition.x - 0.1f, gameObject.transform.localPosition.y);
+       //  foo += 0.0001f;
+        if (move)
+        {
+            gameObject.transform.localPosition = new Vector2(gameObject.transform.localPosition.x + 0.3f * directionScalar, gameObject.transform.localPosition.y); 
+        }
+        //linear
+        //gameObject.transform.localPosition = new Vector2(gameObject.transform.localPosition.x - Mathf.Sin(foo*9) - 1, gameObject.transform.localPosition.y);
+        //print(Vector3.Distance(a,b);
+
+
+         //gameObject.transform.position +=  (Mathf.Sin(2 * Mathf.PI * frequency * Time.time) - Mathf.Sin(2 * Mathf.PI * frequency * (Time.time - Time.deltaTime))) * transform.up;
     }
 
 	// Update is called once per frame
@@ -37,10 +59,18 @@ public class Indicator : MonoBehaviour {
 
             else if (collidedWith.name.Contains("Good Zone"))
             {
-                print("You do win!");
+                GameObject spawn;
+
+                spawn = Instantiate(staticIndicator);
+
+                spawn.transform.position = gameObject.transform.position;
+                
+                //print("You do win!");
             }
 
         }
+
+        
 
 	}
 
@@ -49,6 +79,11 @@ public class Indicator : MonoBehaviour {
         
         collidedWith = other.gameObject;
         //print(collidedWith.name);
+
+        if (collidedWith.name.Contains("Side Trigger"))
+        {
+            directionScalar = directionScalar * (-1);
+        }
 
     }
 
@@ -59,5 +94,12 @@ public class Indicator : MonoBehaviour {
             collidedWith = null;
         }
 
+    }
+
+    IEnumerator StartMove()
+    {
+        yield return new WaitForSeconds(1f);
+
+        move = true;
     }
 }
